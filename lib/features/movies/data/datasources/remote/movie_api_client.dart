1363fac2e33bad965c4fd45488d6684f118d13d1
jbedoyav <jbedoyav@grupo-exito.com>
2024-05-31 13:53:56 -0500

    📦 NEW: Add exception custom and try catch

--- lib/features/movies/data/datasources/remote/movie_api_client.dart
+++ lib/features/movies/data/datasources/remote/movie_api_client.dart
@@ -1,11 +1,23 @@
+import 'package:dio/dio.dart';
+import 'package:reto_28_05_2024/core/exceptions/custom_exception.dart';
 import 'package:reto_28_05_2024/core/utils/base_api.dart';
 
 class MovieApiClient extends BaseApi {
   BaseApi baseApi = BaseApi();
 
   Future<Map<String, dynamic>> getMoviesApi(String params) async {
-    final response = await baseApi.get(
-        'https://api.themoviedb.org/3/movie/$params?api_key=870e936f35b26f8eef70dbc59f3d934f');
-    return response.data;
+    try {
+      final response = await baseApi.get(
+          'https://api.themoviedb.org/3/movie/$params?api_key=870e936f35b26f8eef70dbc59f3d934f');
+      return response.data;
+    } on DioException catch (e) {
+      throw CustomException(
+          messageUser: 'Error de conexión',
+          internalErrorCode: '1',
+          internalErrorMessage: 'Error de conexión',
+          stackTrace: e.stackTrace,
+          data: e.response?.data,
+          httpErrorCode: e.response?.statusCode);
+    }
   }
 }
