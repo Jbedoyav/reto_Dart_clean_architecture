c8ce5279b20af7ea6d219c9f17f9947a26f4b435
jbedoyav <jbedoyav@grupo-exito.com>
2024-06-04 23:18:42 -0500

    fix: injection container name

---
+++ lib/injection_container.dart
@@ -0,0 +1,23 @@
+import 'package:get_it/get_it.dart';
+import 'package:reto_28_05_2024/core/utils/base_api.dart';
+import 'package:reto_28_05_2024/features/movies/data/repositories/movie_repository_impl.dart';
+import 'package:reto_28_05_2024/features/movies/domain/repositories/movie_repository.dart';
+import 'package:reto_28_05_2024/features/movies/domain/usecases/get_movies_now_use_case.dart';
+import 'package:reto_28_05_2024/features/movies/presentation/controllers/movies_now_controller.dart';
+import 'package:reto_28_05_2024/features/movies/presentation/controllers/movies_popular_controller.dart';
+
+final getIt = GetIt.instance;
+
+void injectionContainer() {
+  //Data layer injection container
+  getIt.registerSingleton(BaseApi());
+  getIt.registerFactory<MovieRepository>(() => MovieRepositoryImpl());
+
+  //Domain layer injection container
+  getIt.registerFactory<GetMoviesNowUseCase>(
+      () => GetMoviesNowUseCase(MovieRepositoryImpl()));
+
+  //Presentation layer injection container
+  getIt.registerLazySingleton(() => MoviesNowController());
+  getIt.registerLazySingleton(() => MoviesPopularController());
+}
